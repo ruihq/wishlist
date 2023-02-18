026f52a3ff6c7219113a17552e2ae2e8bba689db
Lumine <120704613+ruihq@users.noreply.github.com>
2023-02-19 01:20:44 +0800

    Create Wishlist.js

---
+++ src/components/Wishlist.js
@@ -0,0 +1,40 @@
+// Code for the Wishlist component, located at src/components/Wishlist.js
+import React, { useState } from 'react';
+
+function Wishlist() {
+  const [items, setItems] = useState([]);
+
+  const addItem = (item) => {
+    setItems([...items, item]);
+  };
+
+  const removeItem = (index) => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+  };
+
+  return (
+    <div>
+      <h1>My Wishlist</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => removeItem(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        addItem(e.target.item.value);
+        e.target.item.value = '';
+      }}>
+        <input name="item" type="text" placeholder="Add item to wishlist" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default Wishlist;
